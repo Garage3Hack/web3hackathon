80d25863e3cf2ed66234c24589956fb967b4a9e4
dtc-NaokiSomeya <nsomeya@tohmatsu.co.jp>
2023-03-18 14:40:34 +0900

    Add feature addLike

--- frontend/pages/IssueDetails/[pjdao_addr]/[issue].tsx
+++ frontend/pages/IssueDetails/[pjdao_addr]/[issue].tsx
@@ -1,4 +1,5 @@
-import { useMemberRegistry, usePjDao, usePjDaoGetAllMembers, usePjDaoGetIssueInfo } from '@/contracts/generated';
+import useDebounce from '@/common/useDebounce';
+import { useMemberRegistry, usePjDao, usePjDaoAddIssue, usePjDaoAddLike, usePjDaoChangeIssueStatus, usePjDaoGetAllMembers, usePjDaoGetIssueInfo, usePreparePjDaoAddLike, usePreparePjDaoChangeIssueStatus } from '@/contracts/generated';
 import { BigNumber } from 'ethers';
 import type { NextPage } from 'next'
 import Head from 'next/head'
@@ -6,12 +7,50 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import { useProvider } from 'wagmi';
+import { useAccount, useProvider, useSigner } from 'wagmi';
 
 const IssueDetails: NextPage = () => {
+    const account = useAccount()
     const router = useRouter();
     const provider = useProvider()
     const { pjdao_addr, issue } = router.query;
+
+    // change status
+    const status2str = (_status: number | undefined) => {
+        if (_status == 0) {
+            return "ToDo";
+        } else if (_status == 1) {
+            return "InProgress";
+        } else if (_status == 2) {
+            return "Done";
+        } else if (_status == 3) {
+            return "Pending";
+        } else {
+            return 'Invalid'
+        }
+    }
+    const status2number = (_status: string | undefined) => {
+        if (_status == "ToDo") {
+            return 0;
+        } else if (_status == "InProgress") {
+            return 1;
+        } else if (_status == "Done") {
+            return 2;
+        } else if (_status == "Pending") {
+            return 3;
+        } else {
+            return -1
+        }
+    }
+    const [issueStatus, setIssueStatus] = useState(0)
+    const debouncedIssueStatus = useDebounce(issueStatus, 500)
+
+    const changeIssueStatusConfig = usePreparePjDaoChangeIssueStatus({
+        address: pjdao_addr as `0x${string}`,
+        args: [BigNumber.from(issue), debouncedIssueStatus]
+    })
+
+    const changeIssueStatusTx = usePjDaoChangeIssueStatus(changeIssueStatusConfig.config)
 
     const pjDaoContract = usePjDao({
         address: pjdao_addr as `0x${string}`,
@@ -24,7 +63,7 @@ const IssueDetails: NextPage = () => {
         args: [BigNumber.from(issue ? issue : 0)]
         }
     )
-    // member 一覧
+    // member 一覧 / add like
     const pjDaoMembers = usePjDaoGetAllMembers({
         address: pjdao_addr as `0x${string}`
     })
@@ -35,8 +74,6 @@ const IssueDetails: NextPage = () => {
         address: process.env.NEXT_PUBLIC_MEMBERREGISTRY_ADDR as `0x${string}` | undefined,
         signerOrProvider: provider
     })
-
-
 
     const [members, setMembers] = useState<any[]>([])
     useEffect( () => {
@@ -49,6 +86,7 @@ const IssueDetails: NextPage = () => {
                     const mem =  await memberRegistryContract.getMember(memberAddr)
                     const mem2 =  await pjDaoContract.members(memberAddr)
                     mems.push({
+                        addr: memberAddr,
                         name: mem![0],
                         introduction: mem![1],
                         skills: mem![2],
@@ -60,12 +98,19 @@ const IssueDetails: NextPage = () => {
             }
         }
         fetchMembers()
-    }, [daoMemberAddrs, memberRegistryContract])
-    const json = [
-        { id: 1, comment: 'AAAAAAAA', issuer: 'aikei', update: '2023/3/17' },
-        { id: 2, comment: 'BBBBBBBB', issuer: 'someya', update: '2023/3/17' },
-        { id: 3, comment: 'CCCCCCCC', issuer: 'koizumi', update: '2023/3/17' },
-    ]
+    }, [daoMemberAddrs, memberRegistryContract, pjDaoContract])
+
+    // add like
+    const [likeMember, setLikeMember] = useState<`0x${string}`>(account.address!)
+    const debouncedLikeMember = useDebounce(likeMember, 500)
+
+    const addLikeConfig = usePreparePjDaoAddLike({
+        address: pjdao_addr as `0x${string}`,
+        args: [BigNumber.from(issue), debouncedLikeMember]
+    })
+
+    const addLikeTx = usePjDaoAddLike(addLikeConfig.config)
+
     return (
         <div>
             <Head><title>Issue Details</title></Head>
@@ -76,33 +121,46 @@ const IssueDetails: NextPage = () => {
                         This page manages issues within the LuiDAO. Please add your comments on the results of your review of this issue.
                     </p>
                 </div>
-                <div className="row row-cols-1 row-cols-md-3 g-4">
+                <div className="row row-cols-1 row-cols-md-2 g-4">
                     <div className="col">
                         <div className="mb-3">
-                            {pjdao_addr}
+                            {/* {pjdao_addr}
                             {issue}
-                            {JSON.stringify(issueDetail.data)}
+                            {JSON.stringify(issueDetail.data)} */}
                             <label className="form-label">Issue Name</label>
                             <input type="text" className="form-control" id="issueName"
-                                placeholder="Enter your issue." />
+                                placeholder="Enter your issue." value={issueDetail?.data?.[0]} disabled />
                         </div>
                         <div className="mb-3">
                             <label className="form-label">Description</label>
                             <textarea className="form-control" id="issueDescription"
-                                placeholder="Enter a description of the issue." rows={3}></textarea>
+                                placeholder="Enter a description of the issue." rows={7} value={issueDetail?.data?.[1]} disabled ></textarea>
                         </div>
                         <div>
                             <label className="form-label">Responsible</label>
                             <input type="text" className="form-control" id="issueResponsible"
-                                placeholder="Enter the name of the person responsible." />
+                                placeholder="Enter the name of the person responsible." value={issueDetail?.data?.[2]} disabled />
                         </div>
                         <div className="mb-3">
-                            <label className="form-label">Status</label>
-                            <input type="text" className="form-control" id="issueStatus"
-                                placeholder="Enter the status of this issue." />
+                            <label className="form-label">Status | Current Status: {status2str(issueDetail?.data?.[3])}</label>
+                            <select className="form-select" id="issueStatus" aria-label="Default select example multiple"
+                                defaultValue={status2str(issueDetail?.data?.[3])}
+                                onChange={(e) => { 
+                                    console.log(e.target.value)
+                                    setIssueStatus(status2number(e.target.value))
+                                }}>
+                                <option>ToDo</option>
+                                <option>InProgress</option>
+                                <option>Done</option>
+                                <option>Pending</option>
+                            </select>
+                            <button type="button" className="btn btn-light" onClick={() => {
+                                changeIssueStatusTx.write?.()
+                            }}>Change status</button>
                         </div>
                         <div className="row" style={{ padding: "1.5rem" }}>
                             {/* メンバー一覧 */}
+                        {issueDetail?.data?.[3] == 2 ? 
                         <div className="card text-dark">
                             <div className="card-header">Members</div>
                             <div className="card-body">
@@ -122,44 +180,44 @@ const IssueDetails: NextPage = () => {
                                                 <td>{member.name}</td>
                                                 <td>{member.role}</td>
                                                 <td>
-                                                <button type="button" className="btn btn-light"><Link href={`/`}><Image alt="refer" src="/icons/eye.svg" width="16" height="16" /></Link></button>
+                                                <button type="button" className="btn btn-light" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                                                    onClick={() => {
+                                                        setLikeMember(member.addr)
+                                                    }}>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
+                                                      <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
+                                                    </svg>
+                                                </button>
                                                 </td>
                                             </tr>
                                         ))}
                                     </tbody>
                                 </table>
                             </div>
+                        </div>: null
+                        }
                         </div>
                     </div>
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Comment</th>
-                                    <th scope="col">Issuer</th>
-                                    <th scope="col">Update</th>
-                                    <th scope="col"></th>
-                                    <th scope="col">Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {json.map(comment => (
-                                    <tr>
-                                        <th scope="row">{comment.id}</th>
-                                        <td>{comment.comment}</td>
-                                        <td>{comment.issuer}</td>
-                                        <td>{comment.update}</td>
-                                        <td>
-                                            <button type="button" className="btn btn-primary"><i className="bi bi-eye"></i></button>
-                                            <button type="button" className="btn btn-danger"><i className="bi bi-trash3"></i></button>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                        <button className="btn btn-primary" type="button">Add</button>
-                    </div>
+               </div>
+            </div>
+            {/* <!-- Modal --> */}
+            <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div className="modal-body text-secondary">
+                    Do you add like to {likeMember} ? 
+                  </div>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-primary" onClick={() => {
+                        addLikeTx.write?.()
+                    }}>Yes</button>
+                  </div>
                 </div>
+              </div>
             </div>
         </div>
     )
